b934b0835d48dadf5fafb1c03a4ca0c31cee1c13
github miyamura_a <github miyamura.a@example.com>
2020-12-16 22:42:06 +0900

    resolve conflict

--- react-app/src/core/index.tsx
+++ react-app/src/core/index.tsx
@@ -5,6 +5,7 @@ import { Link } from 'react-router-dom';
 const tmp = () => {
   console.log('hello world');
   console.log('ccccccccc');
+<<<<<<< HEAD
   1;
   1;
   1;
@@ -22,4 +23,20 @@ const tmp = () => {
   1;
   1;
   1;
+=======
+  2;
+  2;
+  2;
+  2;
+  2;
+  2;
+  22;
+  2;
+  2;
+  2;
+  2;
+  2;
+  2;
+  22;
+>>>>>>> 6d70fe7... testrebase2
 };
